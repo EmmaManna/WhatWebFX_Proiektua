772f248a1798004e918b0a55ae90b1832012550a
Jon Gondra <jongondra22@gmail.com>
2020-11-16 18:12:45 +0100

    Inserten artxiboa irakurri

--- src/main/java/ehu/isad/controllers/db/WhatWebKud.java
+++ src/main/java/ehu/isad/controllers/db/WhatWebKud.java
@@ -1,4 +1,26 @@
 package ehu.isad.controllers.db;
 
+import ehu.isad.utils.Utils;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class WhatWebKud {
+
+    private WhatWebKud(){
+    }
+
+    public void insertIrakur(String artx) throws IOException {
+        FileReader fileR = new FileReader(artx);
+        DBKudeatzaile dbKud = DBKudeatzaile.getInstantzia();
+        try(BufferedReader bufferedReader = new BufferedReader(fileR)){
+            String lerroa;
+            while((lerroa=bufferedReader.readLine())!=null){
+                dbKud.execSQL(lerroa.replace("IGNORE","OR IGNORE"));
+            }
+        }
+    Utils.ezabatu(artx);
+    }
 }
--- src/main/java/ehu/isad/utils/Utils.java
+++ src/main/java/ehu/isad/utils/Utils.java
@@ -1,5 +1,6 @@
 package ehu.isad.utils;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -18,6 +19,17 @@ public class Utils {
         }
 
         return properties;
+    }
+
+    public static final boolean ezabatu(String path) {
+        if(path==null){
+            return true;
+        }
+        File file = new File(path);
+        if(file.exists()){
+            return file.delete();
+        }
+        return true;
     }
 }
 
